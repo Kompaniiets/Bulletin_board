a909755eb77879915f5012aa34190e7c9943cb16
Kompaniiets <igorkompaniets13@mail.ru>
2017-07-03 00:26:25 +0300

    add Update and refactor old code

--- server.js
+++ server.js
@@ -25,19 +25,21 @@ router.route('/login')
         var email = req.body.email;
         var password = req.body.passw;
 
-        connect.query('SELECT * FROM users WHERE email=?', email, function (err, user) {
+        connect.query('SELECT * FROM users WHERE email=?', email, function (err, users) {
             if (err) throw err;
+
+            var user = users[0];
 
             if (!user) {
                 res.json({Body: {success: false, message: 'Authentication failed. User not found.'}});
             }
-            else if (user) {
-                if (user[0].password !== password) {
+            else {
+                if (user.password !== password) {
                     res.status(422, "Unprocessable Entity");
                     res.json({Body: {field: "password", message: "Wrong email or password"}});
                 }
                 else {
-                    var token = jwt.sign({id: user[0].uid}, app.get('secret'), {
+                    var token = jwt.sign({id: user.uid}, app.get('secret'), {
                         expiresIn: '3h' // expires in 3 hours
                     });
 
@@ -57,23 +59,24 @@ router.route('/register')
         var password = req.body.passw;
         var phone = req.body.phone;
 
-        connect.query('SELECT * FROM users WHERE email=?', email, function (err, user) {
+        connect.query('SELECT * FROM users WHERE email=?', email, function (err, users) {
             if (err) throw err;
 
-            if (user[0]) {
+            var user = users[0];
+
+            if (user) {
                 res.json({Body: {success: false, message: 'User already exist'}});
             }
-            else if (user[0] == undefined) {
-                //var sql = 'INSERT INTO users (username, email, password, phone) VALUES ?';
-                var value = {username: name, email: email, password: password, phone: phone};
-                console.log('inside');
-                connect.query('INSERT INTO users SET ?', value, function (err, result) {
 
+            else {
+
+                var sql = 'INSERT INTO users SET ?';
+                var value = {username: name, email: email, password: password, phone: phone};
+
+                connect.query(sql, value, function (err, result) {
                     if (err) throw err;
 
-                    console.log(result);
-
-                    var token = jwt.sign({data: result}, app.get('secret'), {
+                    var token = jwt.sign({id: result.insertId}, app.get('secret'), {
                         expiresIn: '3h' // expires in 3 hours
                     });
 
@@ -118,22 +121,108 @@ router.route('/me')
 
         var userId = req.decoded.id;
 
-        connect.query('SELECT * FROM users WHERE uid = ?', userId, function (err, user) {
+        connect.query('SELECT * FROM users WHERE uid = ?', userId, function (err, users) {
             if (err) throw err;
 
-            else if (user) {
-                res.status(200, 'OK')
+            var user = users[0];
+
+            if (user) {
+                res.status(200, 'OK');
                 res.json(
                     {
-                        'id': user[0].uid,
-                        'phone': user[0].phone,
-                        'name': user[0].username,
-                        'email': user[0].email
+                        'id': user.uid,
+                        'phone': user.phone,
+                        'name': user.username,
+                        'email': user.email
                     });
             }
             else {
                 res.status(401, 'Unauthorized');
                 res.json('empty');
+            }
+        });
+    });
+
+// Update current user
+router.route('/me')
+    .put(function (req, res) {
+
+        var userId = req.decoded.id;
+
+        connect.query('SELECT * FROM users WHERE uid = ?', userId, function (err, users) {
+            if (err) throw err;
+
+            var user = users[0];
+
+            if (!user) {
+                res.status(404, 'User not found');
+                return;
+            }
+
+            var newUser = Object.assign({}, user);
+
+            if (req.body.phone && user.phone != req.body.phone) {
+                newUser.phone = req.body.phone;
+            }
+            if (req.body.name && user.username != req.body.name) {
+                newUser.username = req.body.name;
+            }
+            if (req.body.current_password) {
+                if (user.password === req.body.current_password) {
+                    if (req.body.new_password) {
+                        newUser.password = req.body.new_password;
+                    }
+                    else {
+                        res.json("new_password is empty");
+                    }
+                }
+                else {
+                    res.status(422, 'Unprocessable Entity');
+                    res.json({'field': 'current_password', 'message': 'Wrong current password'});
+                }
+            }
+            if (req.body.email && user.email != req.body.email) {
+                connect.query('SELECT * FROM users WHERE email = ?', req.body.email, function (err, values) {
+                    if (err) throw err;
+
+                    var value = values[0];
+
+                    if (value) {
+                        res.json("user with this email already exist");
+                    }
+                    else {
+                        newUser.email = req.body.email;
+
+                        var sql = 'UPDATE users SET phone=?, username=?, email=?, password=? WHERE uid=?';
+                        connect.query(sql, [newUser.phone, newUser.username, newUser.email, newUser.password, newUser.uid],
+                            function (err, updateUser) {
+                                if (err) throw err;
+
+                                res.json(
+                                    {
+                                        'id': newUser.uid,
+                                        'phone': newUser.phone,
+                                        'name': newUser.username,
+                                        'email': newUser.email
+                                    });
+                        });
+                    }
+                })
+            }
+            else {
+                var sql = 'UPDATE users SET phone=?, username=?, email=?, password=? WHERE uid=?';
+                connect.query(sql, [newUser.phone, newUser.username, newUser.email, newUser.password, newUser.uid],
+                    function (err, updateUser) {
+                        if (err) throw err;
+
+                        res.json(
+                            {
+                                'id': newUser.uid,
+                                'phone': newUser.phone,
+                                'name': newUser.username,
+                                'email': newUser.email
+                            });
+                    });
             }
         });
     });
