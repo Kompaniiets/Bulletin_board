af89fefa8d73d6864f9452e36d7f8479bb481769
Kompaniiets <igorkompaniets13@mail.ru>
2017-07-02 15:05:25 +0300

    create /me router

--- server.js
+++ server.js
@@ -18,45 +18,84 @@ app.use(bodyParser.urlencoded({
 // an instance of the router for api routes
 var router = express.Router();
 
-// login user
+// Login user
 router.route('/login')
     .post(function (req, res) {
 
-        connect.query('SELECT * FROM users WHERE email=?', [req.body.email],  function (err, user) {
-            if(err) throw err;
+        var email = req.body.email;
+        var password = req.body.passw;
 
-            if(!user) {
-                res.json({Body : {success: false, message: 'Authentication failed. User not found.'}});
+        connect.query('SELECT * FROM users WHERE email=?', email, function (err, user) {
+            if (err) throw err;
+
+            if (!user) {
+                res.json({Body: {success: false, message: 'Authentication failed. User not found.'}});
             }
-            else if(user){
-                if(user[0].password !== req.body.passw){
+            else if (user) {
+                if (user[0].password !== password) {
                     res.status(422, "Unprocessable Entity");
-                    res.json({Body : {field: "password", message: "Wrong email or password"}});
+                    res.json({Body: {field: "password", message: "Wrong email or password"}});
                 }
                 else {
-                    //var token = jwt.sign({ foo: 'bar' }, 'secret');
-                    var token = jwt.sign({ data: user }, app.get('secret'), {
-                        expiresIn: '2h' // expires in 3 hours
+                    var token = jwt.sign({id: user[0].uid}, app.get('secret'), {
+                        expiresIn: '3h' // expires in 3 hours
                     });
 
                     res.status(200, "OK");
-                    res.json({Body : {token: token}});
+                    res.json({token: token});
                 }
             }
         });
     });
 
-// route middleware to verify a token
+// Register user
+router.route('/register')
+    .post(function (req, res) {
+
+        var name = req.body.name;
+        var email = req.body.email;
+        var password = req.body.passw;
+        var phone = req.body.phone;
+
+        connect.query('SELECT * FROM users WHERE email=?', email, function (err, user) {
+            if (err) throw err;
+
+            if (user[0]) {
+                res.json({Body: {success: false, message: 'User already exist'}});
+            }
+            else if (user[0] == undefined) {
+                //var sql = 'INSERT INTO users (username, email, password, phone) VALUES ?';
+                var value = {username: name, email: email, password: password, phone: phone};
+                console.log('inside');
+                connect.query('INSERT INTO users SET ?', value, function (err, result) {
+
+                    if (err) throw err;
+
+                    console.log(result);
+
+                    var token = jwt.sign({data: result}, app.get('secret'), {
+                        expiresIn: '3h' // expires in 3 hours
+                    });
+
+                    res.status(200, "OK");
+                    res.json({token: token});
+                })
+            }
+        });
+
+    });
+
+// Route middleware to verify a token
 router.use(function (req, res, next) {
 
     // check header or url parameters or post parameters for token
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
     // decode token
-    if(token){
+    if (token) {
         jwt.verify(token, app.get('secret'), function (err, decoded) {
-            if(err) {
-                res.json({ success: false, message: 'Failed to authenticate token.' });
+            if (err) {
+                res.json({success: false, message: 'Failed to authenticate token.'});
             }
             else {
                 req.decoded = decoded;
@@ -73,9 +112,31 @@ router.use(function (req, res, next) {
     }
 });
 
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
+// Get information about the current user
+router.route('/me')
+    .get(function (req, res) {
+
+        var userId = req.decoded.id;
+
+        connect.query('SELECT * FROM users WHERE uid = ?', userId, function (err, user) {
+            if (err) throw err;
+
+            else if (user) {
+                res.status(200, 'OK')
+                res.json(
+                    {
+                        'id': user[0].uid,
+                        'phone': user[0].phone,
+                        'name': user[0].username,
+                        'email': user[0].email
+                    });
+            }
+            else {
+                res.status(401, 'Unauthorized');
+                res.json('empty');
+            }
+        });
+    });
 
 app.use('/api', router);
 
